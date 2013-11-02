82c3d9a1437065b71be95e35b230bef600b25873
Oscar Robinson <oscarrobinson@btinternet.com>
2013-11-02 22:25:06 +0000

    Turned data display into a table.  Centred all content and added nice header and background

--- datalist.php
+++ datalist.php
@@ -3,10 +3,26 @@ ini_set('display_errors', 'On');
 include_once "datamanager.php";
 $dataManager = new DataManager();
 $stringsArray=$dataManager->getRandomStrings();
-$output = "";
+$output = "<table>";
+$count = 0;
+$columns = 6;
 foreach($stringsArray as $string)
 {
-    $output = $output.$string."<br />";
+    if ($count%$columns==0)
+    {
+        $output = $output."<tr>";
+    }   
+    $output = $output."<td>".$string."</td>";
+    $count = $count + 1;
+    if ($count%$columns==0)
+    {
+        $output = $output."</tr>";
+    }   
 }
+if ($count%$columns!=0)
+{
+        $output = $output."</tr>";
+}
+$output = $output."</table>";  
 echo $output;
 ?>
--- index.php
+++ index.php
@@ -3,6 +3,43 @@
 <head>
 <title>Sensor Data</title>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Chivo:400,900' rel='stylesheet' type='text/css'>
+<style>
+h1{
+	font-family: 'Chivo', sans-serif; text-shadow: 1px 1px 1px #BFBFBF; text-align: center;  
+}
+html {
+
+    height: 100%;
+}
+body{
+	font-family: 'Arial', sans-serif;
+	background: -webkit-linear-gradient(white, #D9D9D9); /* For Safari */
+    background: -o-linear-gradient(white, #D9D9D9); /* For Opera 11.1 to 12.0 */
+    background: -moz-linear-gradient(white, #D9D9D9); /* For Firefox 3.6 to 15 */
+    background: linear-gradient(white, #D9D9D9); /* Standard syntax */
+    height: 100%;
+    margin: 0;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}
+#dataList{
+	width: 900px;
+	margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+}
+#title{
+	width: 500px;
+	margin-left: auto;
+    margin-right: auto;
+}
+table
+{ 
+margin-left: auto;
+margin-right: auto;
+}
+</style>
 <script type="text/javascript">
     $(document).ready(function()
     {
@@ -19,6 +56,7 @@
 
 </head>
 <body>
+<div id="title"><h1>Random Strings</h1></div>
 <div id="dataList"></div>
 </body>
 </html>
