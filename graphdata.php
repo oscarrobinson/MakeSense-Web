d10cc00d009d1913708babb5123cd8ce20888be8
Oscar Robinson <oscarrobinson@btinternet.com>
2013-11-24 19:28:55 +0000

    changed graph to area plot

--- graphdata.php
+++ graphdata.php
@@ -9,16 +9,37 @@ $graphScript = "<script>
 $(function () {
         $('#graph').highcharts({
             chart: {
+            	zoomType: 'x',
                 type: 'spline'
             },
             title: {
                 text: 'Light Sensor Reading'
             },
             plotOptions: {
-    			series: {
-        			animation: false
-    			}
-			},
+                area: {
+                    fillColor: {
+                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    lineWidth: 1,
+                    marker: {
+                        enabled: false
+                    },
+                    shadow: false,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                },
+                series: {
+                	animation: false
+                }
+            },
 
             xAxis: {
                 type: 'datetime',
@@ -38,6 +59,7 @@ $(function () {
             
             series: [{
                 name: 'Light Sensor Data',
+                type: 'area',
                 data: ".$dataManager->graphStringFromDataArray($dataArray)."
           }]
         });
