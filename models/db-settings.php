e432d3bf2344cf84f1ff02f7b3c5844abdb932f5
andrewgrex <andrewgrex@hotmail.co.uk>
2014-03-05 14:14:44 +0000

    edited db-settings, added localhost option

--- models/db-settings.php
+++ models/db-settings.php
@@ -3,12 +3,22 @@
 UserCake Version: 2.0.2
 http://usercake.com
 */
-//Database=makesensemain;Data Source=eu-cdbr-azure-north-b.cloudapp.net;User Id=b56834bde0c85e;Password=87a230d7
+
 //Database Information
+
+/**********For Azure Login**************/
 $db_host = "eu-cdbr-azure-north-b.cloudapp.net"; //Host address (most likely localhost)
 $db_name = "makesensemain"; //Name of Database
 $db_user = "b56834bde0c85e"; //Name of database user
 $db_pass = "87a230d7"; //Password for database user
+
+
+/**********For Localhost Login********** 
+$db_host = "localhost"; //Host address (most likely localhost)
+$db_name = "usercakedb"; //Name of Database
+$db_user = "grex"; //Name of database user
+$db_pass = "moop"; //Password for database user
+*/
 $db_table_prefix = "uc_";
 
 GLOBAL $errors;
