87fadaae22cc6296bdb3189a3a65b6b0bd0aeab5
andrewgrex <andrewgrex@hotmail.co.uk>
2014-03-05 11:37:37 +0000

    adding root user

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -10,5 +10,8 @@ $cfg['Servers'][$i]['host'] = 'eu-cdbr-azure-north-b.cloudapp.net';  // Replace 
 $cfg['Servers'][$i]['connect_type'] = 'tcp';  
 $cfg['Servers'][$i]['compress'] = false;  
 $cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['user'] = 'root';  
+$cfg['Servers'][$i]['password'] = '';
 $cfg['Servers'][$i]['AllowNoPassword'] = true; 
+
 ?>  
