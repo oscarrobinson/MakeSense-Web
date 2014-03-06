82fc474c49cf2bab611657581ffe96f6befc97b9
Oscar Robinson <oscarrobinson@btinternet.com>
2014-03-06 12:45:04 +0000

    changed footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 echo"
 <div id='footer'>
       <div class='container'>
-        <p class='text-muted credit'>This website is pretty <a href='http://shibe-doge.tumblr.com/'>awesome</a>. Here is a footer motherfuckers</p>
+        <p>© MakeSense 2014</p>
       </div>
     </div>
 "
