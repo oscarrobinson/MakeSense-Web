19d639beb184dd0c86ca42dc17690a1bfb7c3a26
andrewgrex <andrewgrex@hotmail.co.uk>
2014-03-15 02:52:30 +0000

    updated about page

--- contact.php
+++ contact.php
@@ -16,30 +16,34 @@ echo "
 <div id='wrap'>
 	<div id='content'>
 		<div id=\"title\"><h1>Contact</h1></div>
-	<br><br><br>
+
 	</div>
 
+	<div class='container'>
+		<h3>For further information or any queries you may have, contact us today at &lt;contact@make-sense.co.uk&gt;</h3>
+	</div>
+	
+	<br><br>
 	<div class='container marketing'>
-
 		<!-- Three columns of text below the carousel -->
 		<div class='row'>
 			<div class='col-lg-4'>
 				<img class='img-circle' src='img/andy.jpg' data-src='holder.js/140x140' alt='Generic placeholder image' >
 				<h2>Andrew Greatorex</h2>
-				<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-				<p><a class='btn btn-default' href='#'>View details &raquo;</a></p>
+				<p>A second year UCL Computer Science student. Interests include long walks on the beach and bubble baths</p>
+				<!--<p><a class='btn btn-default' href='#'>View details &raquo;</a></p>-->
 			</div><!-- /.col-lg-4 -->
 			<div class='col-lg-4'>
 				<img class='img-circle' src='img/oscar.jpg' data-src='holder.js/140x140' alt='Generic placeholder image' >
 				<h2>Oscar Robinson</h2>
-				<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-				<p><a class='btn btn-default' href='#'>View details &raquo;</a></p>
+				<p>A second year UCL Computer Science student. Interests include stamp collecting, competitive dog grooming and sculpting egg shells</p>
+				<!--<p><a class='btn btn-default' href='#'>View details &raquo;</a></p>-->
 			</div><!-- /.col-lg-4 -->
 			<div class='col-lg-4'>
 				<img class='img-circle' src='img/jay.jpg' data-src='holder.js/140x140' alt='Generic placeholder image'>
 				<h2>Jay Shah</h2>
-				<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-				<p><a class='btn btn-default' href='#'>View details &raquo;</a></p>
+				<p>A second year UCL Computer Science student. Interest include shaping hedges and trainspotting </p>
+				<!--<p><a class='btn btn-default' href='#'>View details &raquo;</a></p>-->
 			</div><!-- /.col-lg-4 -->
 		</div><!-- /.row -->
 	</div>
