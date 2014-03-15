b99e0ace989ddf93d336e7c0df1f811c32daa352
andrewgrex <andrewgrex@hotmail.co.uk>
2014-03-15 03:03:22 +0000

    updated index page

--- index.php
+++ index.php
@@ -26,8 +26,8 @@ echo "
 				<img src='img/makesense.png' data-src='holder.js/100%x500/auto/#777:#7a7a7a/text:First slide' alt='First slide'>
 				<div class='container'>
 					<div class='carousel-caption'>
-						<h1>Such Wireless.</h1>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>";
+						<h1>Sensor Networks</h1>
+						<p>Every network is completely customisable and can have as many types of sensors and number of nodes as you require. The nodes wirelessly transmit data to a central gateway which pushes the information to the MakeSense cloud platform</p>";
 							if(!isUserLoggedIn()){
 							echo"
 							<p><a class='btn btn-large btn-primary' href='login.php'>Sign up today</a></p>";
@@ -40,9 +40,14 @@ echo "
 				<img src='img/suchsense.jpg' data-src='holder.js/100%x500/auto/#777:#7a7a7a/text:Second slide' alt='Second slide'>
 				<div class='container'>
 					<div class='carousel-caption'>
-						<h1>Very Sensors.</h1>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-						<p><a class='btn btn-large btn-primary' href='#'>Learn more</a></p>
+						<h1>User Accounts</h1>
+						<p>Each one of your sensor networks is linked to your account so you can moniter, set alerts and analyse your data. Each account permits as many networks as you require</p>";
+							if(!isUserLoggedIn()){
+							echo"
+							<p><a class='btn btn-large btn-primary' href='login.php'>Sign up today</a></p>";
+							}
+							echo"
+						<!-- <p><a class='btn btn-large btn-primary' href='#'>Learn more</a></p>-->
 					</div>
 				</div>
 			</div>
@@ -50,9 +55,14 @@ echo "
 				<img src='img/hank7.jpg' data-src='holder.js/100%x500/auto/#777:#7a7a7a/text:Third slide' alt='Third slide'>
 				<div class='container'>
 					<div class='carousel-caption'>
-						<h1>Wow.</h1>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-						<p><a class='btn btn-large btn-primary' href='#'>Browse gallery</a></p>
+						<h1>Data Analytics</h1>
+						<p>Using the MakeSense dashboard, you can view statistics on each of your networks. You can customise the dashboard to match your needs and can even set alerts if sensor data exceeds set thresholds</p>";
+							if(!isUserLoggedIn()){
+							echo"
+							<p><a class='btn btn-large btn-primary' href='login.php'>Sign up today</a></p>";
+							}
+							echo"
+						<!-- <p><a class='btn btn-large btn-primary' href='#'>Browse gallery</a></p>-->
 					</div>
 				</div>
 			</div>
