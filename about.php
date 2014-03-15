767424ea1e06b6f682c44778c379c182aa08f7ba
andrewgrex <andrewgrex@hotmail.co.uk>
2014-03-15 02:07:32 +0000

    updated about page

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
-<?php
+o<?php
 /*
 UserCake Version: 2.0.2
 http://usercake.com
@@ -15,37 +15,51 @@ echo "
 <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class='jumbotron'>
       <div class='container'>
-        <h1>Data, data everywhere</h1>
-        <p>Oi you there, fancy a wireless sensor network? 'course you do. Come buy one (courtesy of Hypnotoad) lolz i'm just writing this to fill up space</p>
-        <p><a class='btn btn-primary btn-lg'>Learn more &raquo;</a></p>
-      </div>
+		<div id='sensorinfo'>
+			<h1>Customisable Sensor Networks</h1>
+			<p>MakeSense is an easy to set up, reliable sensor network allowing users to monitor any environmental factor they desire.  Data is viewable using the MakeSense web platform where all sensor data from the user’s network is viewable ready for analysis.  Building managers could use the system to monitor the temperature, light levels and movement in rooms in their buildings and use the data provided to optimise how they heat and light rooms at different times of day.</p>
+			<!--<p><a class='btn btn-primary btn-lg'>Learn more &raquo;</a></p>-->
+		</div>
+	  </div>
     </div>
-
+	
+	<hr>
+    <hr>
+	
+	<div class='jumbotron'>	
+		<div class='container'>
+				<div id='makesensevideo'>
+					<embed
+						width='600dp' height='450dp'
+						src='http://www.youtube.com/v/TMhPiHF3Qzg'
+						type='application/x-shockwave-flash'>
+					</embed>
+				</div>
+		</div>
+	</div>
+	
     <div class='container'>
       <!-- Example row of columns -->
+	  <div id='aboutbottominfo'>
       <div class='row'>
         <div class='col-lg-4'>
-          <h2>Network</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class='btn btn-default' href='#'>View details &raquo;</a></p>
+          <h2>User Accounts</h2>
+          <p>Each one of your sensor networks is linked to your account so you can moniter, set alerts and analyse your data. Each account permits as many networks as you require</p>
+          <!--<p><a class='btn btn-default' href='#'>View details &raquo;</a></p>-->
         </div>
         <div class='col-lg-4'>
-          <h2>Sensors</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class='btn btn-default' href='#'>View details &raquo;</a></p>
+          <h2>Sensor Networks</h2>
+          <p>Every network is completely customisable and can have as many types of sensors and number of nodes as you require. The nodes wirelessly transmit data to a central gateway which pushes the information to the MakeSense cloud platform</p>
+          <!--<p><a class='btn btn-default' href='#'>View details &raquo;</a></p>-->
        </div>
         <div class='col-lg-4'>
-          <h2>Cloud Services</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class='btn btn-default' href='#'>View details &raquo;</a></p>
+          <h2>Data Analytics</h2>
+          <p>Using the MakeSense dashboard, you can view statistics on each of your networks. You can customise the dashboard to match your needs and can even set alerts if sensor data exceeds set thresholds</p>
+          <!--<p><a class='btn btn-default' href='#'>View details &raquo;</a></p>-->
         </div>
+		</div>
       </div>
 
-      <hr>
-
-      <footer>
-        <p>&copy; Company 2013</p>
-      </footer>
     </div> <!-- /container -->    
 </div>";
 	
