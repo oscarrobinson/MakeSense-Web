1c8781ba2c5f7e1229642339437e4dae4a360e25
andrewgrex <andrewgrex@hotmail.co.uk>
2014-03-22 15:07:11 +0000

    changed

--- wiki/LocalSettings.php
+++ wiki/LocalSettings.php
@@ -29,7 +29,7 @@ $wgScriptPath = "/wiki";
 $wgScriptExtension = ".php";
 
 ## The protocol and server name to use in fully-qualified URLs
-$wgServer = "http://localhost";
+$wgServer = "http://uclteam10.azurewebsites.net/";
 
 ## The relative URL path to the skins directory
 $wgStylePath = "$wgScriptPath/skins";
