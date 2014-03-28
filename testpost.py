0a0213f408ba2f89f0f29498d575c25ada1d0858
Oscar Robinson <oscarrobinson@btinternet.com>
2014-03-28 16:33:06 +0000

    removed stuff

--- testpost.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def addSensor(sensorId, netId, sensorOnt):
-	payload = {'sensorId': sensorId, 'netId': netId, 'sensorOnt': sensorOnt}
-	r = requests.post("http://uclteam10.azurewebsites.net/testpost.php", data=payload)
-
-addSensor("thing","thing2","12")
