89fdf1f28f34942ff47d525da05390dd8954af09
Oscar Robinson <oscarrobinson@btinternet.com>
2014-03-28 16:08:34 +0000

    initial testing of python api

--- testpost.php
+++ testpost.php
@@ -1,14 +1,13 @@
 <?php
 include_once "datamanager.php";
-/*
+
 $sensorId = $_POST['sensorId'];
 $netId = $_POST['netId'];
-$sensorOnt = $_POST['sensorOnt'];*/
-$netId = $_POST['netId'];
+$sensorOnt = $_POST['sensorOnt'];
+echo "This page is working :)<br></br>";
+echo $sensorId;
 
 $dataManager = new DataManager();
-//$dataManager->addSensor($sensorId, $netId, $sensorOnt);
-$thing = $dataManager->getSensorsInNetwork($netId);
-echo $thing;
+$dataManager->addSensor($sensorId, $netId, $sensorOnt);
 
 ?>
--- testpost.py
+++ testpost.py
@@ -1,6 +1,5 @@
 import requests
 
-#payload = {'sensorId': 'TESTTHING', 'netId': 'NETTEST', 'sensorOnt': '12'}
-payload = {'netId': '00000000f01243e9'}
+payload = {'sensorId': 'TESTTHING', 'netId': 'NETTEST', 'sensorOnt': '12'}
 r = requests.post("http://uclteam10.azurewebsites.net/testpost.php", data=payload)
 print r.text
