c163711b51d60351ddf11b960b314d8339944012
Oscar Robinson <oscarrobinson@btinternet.com>
2014-03-28 16:14:48 +0000

    initial testing of python api

--- datamanager.php
+++ datamanager.php
@@ -189,7 +189,7 @@ class DataManager
     }
 
     public function addSensor($sensorId, $netId, $sensorOnt){
-        $sql_add = "INSERT INTO sensors(sensor_id, network_id, ontology_id) VALUES(\'".$sensorId."\',\'".$netId."\',\'".$sensorOnt."\')";
+        $sql_add = "INSERT INTO sensors(sensor_id, network_id, ontology_id) VALUES('".$sensorId."','".$netId."','".$sensorOnt."')";
         $stmt = $this->conn->query($sql_add);
         $data = $stmt->fetchAll();
         return;
