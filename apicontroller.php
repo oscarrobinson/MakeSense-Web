ec82e6a7039227c81f44d7bf8f16cb2c6b00e7c7
Oscar Robinson <oscarrobinson@btinternet.com>
2014-04-05 15:00:24 +0100

    added request code functionality

--- apicontroller.php
+++ apicontroller.php
@@ -2,13 +2,22 @@
 ini_set('display_errors', 'On');
 include_once "datamanager.php";
 
-$sensorId = $_POST['sensorId'];
-$netId = $_POST['netId'];
-$sensorOnt = $_POST['sensorOnt'];
+
+$dataManager = new DataManager();
+
+$requestCode = $_POST['requestCode'];
+
+if ($requestCode=="1"){
+	$sensorId = $_POST['sensorId'];
+	$netId = $_POST['netId'];
+	$sensorOnt = $_POST['sensorOnt'];
+	$dataManager->addSensor($sensorId, $netId, $sensorOnt);
+}
+
+
 echo "This page is working :)<br></br>";
 echo $sensorId;
 
-$dataManager = new DataManager();
-$dataManager->addSensor($sensorId, $netId, $sensorOnt);
+
 
 ?>
