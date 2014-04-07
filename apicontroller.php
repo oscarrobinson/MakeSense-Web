016177d05fbe08c6ced336f91c67e204683971b1
Oscar Robinson <oscarrobinson@btinternet.com>
2014-04-07 18:51:04 +0100

    working on api controller

--- apicontroller.php
+++ apicontroller.php
@@ -4,6 +4,8 @@ include_once "datamanager.php";
 
 //ERROR CODES
 $duplicateEntryError = 23000;
+$userValidCode = 1000;
+$userInvalidCode = 1001;
 
 
 $dataManager = new DataManager();
@@ -11,6 +13,18 @@ $dataManager = new DataManager();
 $requestCode = $_POST['requestCode'];
 
 if ($requestCode=="1"){
+	$username = $_POST['username'];
+	$id = $_POST['apiId'];
+	$userValid = $dataManager->validateApiUser($username, $id);
+	if ($userValid){
+		echo $userValidCode;
+	}
+	else{
+		echo $userInvalidCode;
+	}
+}
+
+if ($requestCode=="10"){
 	$sensorId = $_POST['sensorId'];
 	$netId = $_POST['netId'];
 	$sensorOnt = $_POST['sensorOnt'];
